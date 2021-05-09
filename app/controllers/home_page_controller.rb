532b8bff1ab9142051510a414c487044e572caa4
Osamah Mandawi <oamandawi@brandeis.edu>
2021-05-08 21:49:13 -0400

    Adds basic puzzle

--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -1,4 +1,12 @@
 class HomePageController < ApplicationController
   def show
+    @sentence = "chickens like to jump"
+    @guess = params[:guess]
+    @opened_letters = []
+    if @guess.present?
+      if @sentence.include? @guess
+        @opened_letters << @guess       
+      end
+    end
   end
 end

354d85cc375736222a5c823777b3fd7427b279f7
Osamah Mandawi <oamandawi@brandeis.edu>
2021-05-13 22:59:18 -0400

    Improved waiting room mechanics

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,12 +1,28 @@
 require './lib/games_utils'
 class GamesController < ApplicationController
-  before_action :set_game, only: %i[ show edit update destroy join wait ]
+  before_action :set_game, only: %i[ show edit update destroy ]
   before_action :require_login
 
   # GET /games or /games.json
   def index
     @games = Game.all
   end
+
+  # DEPRECATED SHOW
+  # def show
+  #   @sentence = "chickens like to jump"
+  #   @guess = params[:guess]
+  #   @opened_letters = @game.opened_letters
+  #   @score = Score.where(game_id: @game.id, user_id: current_user.id).first
+  #   if @guess.present?
+  #     if @sentence.include? @guess and not @opened_letters.include? @guess
+  #       newscore = @score.total + @sentence.count(@guess)
+  #       @score.update(total: newscore)
+  #       @game.update(opened_letters: @opened_letters << @guess)  
+  #     end
+  #   end
+  #   @total = @score.total
+  # end
 
   # GET /games/1 or /games/1.json
   def show
@@ -16,18 +32,11 @@ class GamesController < ApplicationController
     # - Turns
     # - Track money
     # - Add total money at end of round
-    @sentence = "chickens like to jump"
-    @guess = params[:guess]
-    @opened_letters = @game.opened_letters
-    @score = Score.where(game_id: @game.id, user_id: current_user.id).first
-    if @guess.present?
-      if @sentence.include? @guess and not @opened_letters.include? @guess
-        newscore = @score.total + @sentence.count(@guess)
-        @score.update(total: newscore)
-        @game.update(opened_letters: @opened_letters << @guess)  
-      end
+    if not @game.users.include? current_user
+      GamesUtils.create_game_player(@game, current_user, @game.game_players.last.player_order + 1)
     end
-    @total = @score.total
+    @user = current_user.email.partition('@').first
+    @players = @game.users.map { |user| user.email.partition('@').first }
   end
 
   # GET /games/new
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, controller: "clearance/sessions", only: [:create]
 
+  resources :games
+
   resources :users, only: [:create] do
     resource :password,
       controller: "clearance/passwords",

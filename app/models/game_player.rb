b1ac43c381f04eaeec35c364f86538464a0dbdc3
Osamah Mandawi <oamandawi@brandeis.edu>
2021-05-21 14:53:39 -0400

    Turbo streamed game players in waiting room

--- app/models/game_player.rb
+++ app/models/game_player.rb
@@ -1,5 +1,11 @@
 class GamePlayer < ApplicationRecord
+  include ActionView::RecordIdentifier
+
   belongs_to :game
   belongs_to :user
   default_scope { order(player_order: :asc) }
+
+  after_create_commit -> {
+    broadcast_append_to [game, :game_players], target: "#{dom_id(game)}_game_players"
+  }
 end
